11286b4a8d54f7d7699107ddbd80b4e29520c90d
unknown <hazine505@gmail.com>
2022-06-05 22:27:29 +0300

    поприветствовали Лилит

---
+++ Example005_Hello2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+if (username == "лилит")
+{
+    Console.WriteLine("Приветствуем Ваше Великолепие Лилит!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
